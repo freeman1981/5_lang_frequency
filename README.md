dd669ff03435e320d22130d4c75a96429eca2271
freeman <alexander.i.kamenev@gmail.com>
2017-04-28 20:03:56 +0300

    start

--- README.md
+++ README.md
@@ -1,6 +1,26 @@
 # Frequency Analysis of Words
 
-[TODO. There will be project description]
+The script count words in requested text file and show ten the most frequent words with it frequency,
+ordered by they frequency.
+
+# Как запустить
+
+### Скрипт требует для своей работы установленного интерпретатора Python версии 3.5
+
+Запуск на Linux:
+
+```#!bash
+
+$ python3 lang_frequency.py <path_to_file>
+Ten most frequent words in lang_frequency.py is:
+word "import" was founded 4 times
+word "def" was founded 4 times
+word "words" was founded 3 times
+...
+
+```
+
+Запуск на Windows происходит аналогично.
 
 # Project Goals
 
--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,39 @@
+import argparse
+import sys
+from string import punctuation
+from collections import Counter
 
 
-def load_data(filepath):
-    pass
+def load_data(file_path):
+    with open(file_path, 'r') as file_handler:
+        return file_handler.read()
 
 
-def get_most_frequent_words(text):
-    pass
+def get_ten_most_frequent_words(text: str):
+    translator = str.maketrans('', '', punctuation)
+    translated_text = text.translate(translator).lower()
+    words = translated_text.split()
+    counted_words = Counter(words)
+    return sorted(counted_words.items(), key=lambda item: item[1], reverse=True)[:10]
+
+
+def print_most_frequent_words(dict_items):
+    for dict_item in dict_items:
+        print('word "{}" was founded {} times'.format(*dict_item))
+
+
+def parse_command_line_and_get_it_args():
+    parser = argparse.ArgumentParser(description='Find the most frequent words in file')
+    parser.add_argument('path', type=str, help='path to file')
+    return parser.parse_args()
 
 
 if __name__ == '__main__':
-    pass
+    args = parse_command_line_and_get_it_args()
+    try:
+        text = load_data(args.path)
+    except FileNotFoundError:
+        print('file {file_path} does not exists'.format(file_path=args.path))
+        sys.exit(1)
+    print('Ten most frequent words in {file_path} is:'.format(file_path=args.path))
+    print_most_frequent_words(get_ten_most_frequent_words(text))
